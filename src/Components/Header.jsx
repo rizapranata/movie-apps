3f575185b83c454a44b90e6fc17af4c7e15d2f3f
Riza <riza.ari@amartek.id>
2025-03-05 23:30:53 +0700

    feat: implement selected search

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,16 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import GlobalApi from '../Services/GlobalApi';
 import useMovieStore from '../Services/store';
+import { useNavigate } from "react-router-dom";
+import { Select } from "antd";
 
+const { Option } = Select;
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  const [search, setSearch] = useState("");
   const [genres, setGenres] = useState([]);
+  const [movies, setMovies] = useState([]);
   const { isPositionDetailPage, onChangePosition } = useMovieStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchGenres = async () => {
@@ -19,27 +23,45 @@ function Header() {
       setGenres(data.genres);
     };
 
+    const fetchMovies = async () => {
+      const data = await GlobalApi.getDiscover();
+      setMovies(data.results);
+    }
+
     fetchGenres();
+    fetchMovies();
   }, []);
 
-  console.log("isPositionDetailPage:", isPositionDetailPage);
+  const handleChange = (value) => {
+    navigate(`/detail/movie/${value}`);
+    onChangePosition(true);
+  };
+
+  console.log("movies:", movies);
   return (
     <nav className={`${isPositionDetailPage ? "bg-opacity-50" : "bg-primary"} text-white p-4 fixed top-0 left-0 w-full z-10 sm:px-5 md:px-[20px] lg:px-40 xl:px-80`}>
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo & Search */}
         <div className="flex items-center space-x-4 md:space-x-7">
-          <Link to="/" className="text-xl font-bold" onClick={() => {onChangePosition(false)}}>
+          <Link to="/" className="text-xl font-bold" onClick={() => { onChangePosition(false) }}>
             <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
           </Link>
           {/* Search Input */}
           <div className="relative hidden sm:block">
-            <input
-              type="text"
-              placeholder="Find movie.."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className={`${isPositionDetailPage ? "bg-transparent" : "bg-secondary"} px-4 py-1 pr-10 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-40 sm:w-60 md:w-72 lg:w-80`}
-            />
+            <Select
+              showSearch
+              placeholder="Search and Select.."
+              optionFilterProp="children"
+              onChange={handleChange}
+              style={{ height: '40px' }}
+              className={`${isPositionDetailPage ? "bg-transparent" : "bg-secondary"} py-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-40 sm:w-60 md:w-72 lg:w-80`}
+            >
+              {movies && movies.map(movie => (
+                <Option key={movie.id} value={movie.id} style={{ backgroundColor: 'black', color: 'white', border: '0' }}>
+                  {movie.title}
+                </Option>
+              ))}
+            </Select>
             <img
               src={searchIcon}
               className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4"
